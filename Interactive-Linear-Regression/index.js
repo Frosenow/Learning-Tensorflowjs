2232f51c1d855303da6adaaf26837ca941eda4c2
Frosenow <filip.potepa@gmail.com>
2023-02-17 22:00:03 +0100

    loss, predict, optimizer function added

--- Interactive-Linear-Regression/index.js
+++ Interactive-Linear-Regression/index.js
@@ -1,3 +1,5 @@
+// Interactive Linear Regression using Tensorflow.js and p5.js 
+
 // y = ax + b <- Linear function  
 let a, b 
 
@@ -7,26 +9,46 @@ let y_cords = [];
 const learningRate = 0.5; 
 const optimizer = tf.train.sgd(learningRate)
 
+
 function setup(){
     createCanvas(500, 500); 
-    background(0)
-    // Setting function in random place at first
-    a = tf.scalar(random(1)).variable()
-    b = tf.scalar(random(1)).variable()
+    // Setting function parameters in random place at first
+    // Define slope of y = ax + b
+    a = tf.scalar(Math.random()).variable()
+    // Define y-intercept of y = ax + b 
+    b = tf.scalar(Math.random()).variable()
 }
 
+// Predicts y values to measure the mean square error 
+function predict(x_cords){
+    const x_tensors = tf.tensor1d(x_cords); 
+    // y = ax + b 
+    const y_cords = x_tensors.mul(a).add(b);
+    return y_cords 
+}
+
+function loss(pred, labels){
+    // Calculating loss function using square mean error 
+    return pred.sub(labels).square().mean();
+}
+
+// Creating dataset interactively 
 function mousePressed(){
     let x = map(mouseX, 0, width, 0, 1);
     let y = map(mouseY, 0, height, 1, 0);
 
     x_cords.push(x); 
     y_cords.push(y)
-    console.log(x_cords, y_cords)
 }
 
 function draw(){
+    
+    y_tensor = tf.tensor1d(y_cords);
+    optimizer.minimize(() => loss(predict(x_cords), y_tensor))
+
+    background(0)
     stroke(255)
-    strokeWeight(8)
+    strokeWeight(10)
 
     for(let i = 0; i < x_cords.length; i++){
         let px = map(x_cords[i], 0, 1, 0, width)
